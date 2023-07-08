9d44d9b5a48ea915e9ba73a01b1afa2348d1a15e
Petter Skogh <petter.skogh@xlent.se>
2023-07-08 12:08:41 +0200

    Update server to format quackified author correctly
    * Update server to format quackified author correctly, e.g. capitilze only the first letter of each word

--- src/routes/api/quack/+server.js
+++ src/routes/api/quack/+server.js
@@ -4,24 +4,33 @@ export async function GET() {
   const image = await fetch('https://random-d.uk/api/v2/quack')
     .then(res => res.json())
     .then(data => data.url);
-  const { q: quote, a: author } = await fetch('https://zenquotes.io/api/quotes')
+  const { quote, author } = await fetch('https://zenquotes.io/api/quotes')
     .then(res => res.json())
-    .then(data => data[0]);
+    .then(data => { 
+      return {
+        quote: data[0].q,
+        author: quackifyAuthor(data[0].a)
+      }
+    });
 
-  return json({ 
-    quote,
-    author: replaceBeforeVowel(author, 'Quack'),
-    image 
-  });
+  return json({ quote, author, image });
 }
 
 /**
- * @param { string } str String to replace
- * @param { string } replacement String to replace with
- * @returns New string with replacement before second vowel. 
+ * @param { String } author Name to quackify
+ * @returns Quackified author name
+ */
+const quackifyAuthor = (author) => {
+  return formatCapitals(replaceBeforeVowel(author, 'Quack'));
+}
+
+/**
+ * @param { String } str String to replace
+ * @param { String } replacement String to replace with
+ * @returns { String } New string with replacement before second vowel. 
  * If no second vowel, before first vowel. If no vowels, returns original string.
  */
-function replaceBeforeVowel(str, replacement) {
+const replaceBeforeVowel = (str, replacement) => {
   const match = str.match(/[aeiou]/i);
 
   if(!match) {
@@ -29,4 +38,18 @@ function replaceBeforeVowel(str, replacement) {
   }
 
   return replacement + str.slice(str.indexOf(match[0]));
+}
+
+/**
+ * Formats a string to have the first letter of each word capitalized
+ * @param { String } str String to format
+ * @returns { String } Formatted string with first letter of each word capitalized
+ */
+const formatCapitals = (str) => {
+  var lowercaseString = str.toLowerCase();
+  var words = lowercaseString.split(' ');
+  
+  var formattedWords = words.map((word) => word.charAt(0).toUpperCase() + word.slice(1));
+  
+  return formattedWords.join(' ');
 }
